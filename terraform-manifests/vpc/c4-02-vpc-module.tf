8947fc6408e1c0845a1f60ea4f0d9d9b6fb1fd62
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-06 13:43:21 +0300

    Modulee to create VPC

---
+++ terraform-manifests/vpc/c4-02-vpc-module.tf
@@ -0,0 +1,44 @@
+# VPC Module
+module "three-tier-vpc" {
+    source = "terraform-aws-modules/vpc/aws"
+     
+     # VPC Basic Details
+     name = var.vpc_name
+     cidr = var.vpc_cidr_block
+     azs = var.vpc_availability_zones
+     private_subnets = var.vpc_private_subnets
+     public_subnets = var.vpc_public_subnets
+    
+    # Database subnets
+     database_subnets = var.vpc.database_subnets
+     create_database_subnet_group = var.vpc_create_database_subnet_group
+     create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+ 
+     # NAT Gateways - Outbound Communication
+     enable_nat_gateway = var.vpc_enable_nat_gateway
+     single_nat_gateway = var.vpc_single_nat_gateway
+      
+    # VPC DNS Parameters
+     enable_dns_hostnames = true
+     enable_dns_support   = true
+
+        
+     tags = local.common_tags
+     vpc_tags =  local.common_tags
+
+     # Additional Tags to Subnets
+    public_subnet_tags = {
+    Type = "Public Subnets"
+  }
+    private_subnet_tags = {
+    Type = "Private Subnets"
+  }  
+    database_subnet_tags = {
+    tier = "database"
+    visibility = "private"
+  }
+    enable_flow_log = true
+   flow_log_destination_type = "s3"
+
+
+}
