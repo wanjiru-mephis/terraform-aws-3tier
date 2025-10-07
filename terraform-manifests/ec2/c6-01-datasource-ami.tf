d210e2684a3560755239ed42920c1c1386a7bf6e
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-07 22:44:02 +0300

    Define EC2 Instance files

---
+++ terraform-manifests/ec2/c6-01-datasource-ami.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "amazon_linux_2023" {
+  most_recent      = true
+  owners           = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-minimal-*-arm64"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
