a8375bb8fab5feb86a63e66520ab4294e81236e3
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-07 22:22:16 +0300

    VPC Outputs

--- terraform-manifests/vpc/c4-03-vpc-outputs.tf
+++ terraform-manifests/vpc/c4-03-vpc-outputs.tf
@@ -3,38 +3,44 @@
 #VPC ID
 output "vpc_id" {
     description = "ID of project VPC"
-    value = module.three-tier-vpc.vpc_id
+    value = module.vpc_main.vpc_id
 }
 
 # VPC CIDR Blocks
 output "vpc_cidr_block" {
     description = "The CIDR Block of the VPC"
-    value = module.three-tier-vpc.vpc_cidr_block
+    value = module.vpc_main.vpc_cidr_block
   
 }
 
 # VPC Private Subnets
 output "private_subnets" {
     description = "List of IDs of private subnets"
-    value = module.three-tier-vpc.private_subnets
+    value = module.vpc_main.private_subnets
   
 }
 
 # VPC Public Subnets
 output "public_subnets" {
     description = "List of IDs of public subnets"
-    value = module.three-tier-vpc.public_subnets
+    value = module.vpc_main.vpc.public_subnets
   
 }
 
 # VPC AZs
 output "azs" {
   description = "A list of availability zones spefified as argument to this module"
-  value       = module.three-tier-vpc.azs
+  value       = module.vpc_main.azs
 }
 
 # VPC NAT gateway Public IP
 output "nat_public_ips" {
   description = "List of public Elastic IPs created for AWS NAT Gateway"
-  value       = module.three-tier-vpc.nat_public_ips
+  value       = module.vpc_main.nat_public_ips
+}
+
+# VPC Common tags
+output "common_tags" {
+    description = "Common tags used across modules"
+    value = var.tags
 }
