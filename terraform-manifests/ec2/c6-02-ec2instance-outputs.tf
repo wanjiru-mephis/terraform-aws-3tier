8d81145f921d391432f74938cc852010852c0408
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-08 17:25:16 +0300

    Quick fix

--- terraform-manifests/ec2/c6-02-ec2instance-outputs.tf
+++ terraform-manifests/ec2/c6-02-ec2instance-outputs.tf
@@ -1,19 +1,25 @@
-# AWS EC2 Instance Terraform Outputs
+# -----------------------------
 # Public EC2 Instances - Bastion Host
+# -----------------------------
 output "ec2_bastion_public_instance_ids" {
-  description = "List of IDs of instances"
+  description = "List of IDs of public EC2 instances (Bastion)"
   value       = module.ec2_public.id
 }
+
 output "ec2_bastion_public_ip" {
-  description = "List of Public ip address assigned to the instances"
+  description = "List of public IP addresses of public EC2 instances (Bastion)"
   value       = module.ec2_public.public_ip
 }
+
+# -----------------------------
 # Private EC2 Instances
+# -----------------------------
 output "ec2_private_instance_ids" {
-  description = "List of IDs of instances"
-  value       = module.ec2_private.id
+  description = "List of IDs of private EC2 instances"
+  value       = [for inst in module.ec2_private : inst.id]
 }
+
 output "ec2_private_ip" {
-  description = "List of private ip address assigned to the instances"
-  value       = module.ec2_private.private_ip
+  description = "List of private IP addresses of private EC2 instances"
+  value       = [for inst in module.ec2_private : inst.private_ip]
 }
