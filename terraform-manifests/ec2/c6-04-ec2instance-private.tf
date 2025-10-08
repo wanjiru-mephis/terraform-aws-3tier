795ec254bd0b476dd411f2a09c998f067aaada5f
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-08 17:27:26 +0300

    Fix depends on argment

--- terraform-manifests/ec2/c6-04-ec2instance-private.tf
+++ terraform-manifests/ec2/c6-04-ec2instance-private.tf
@@ -1,18 +1,19 @@
 # EC2 InstanceS created in VPC Private Subnets
 module "ec2_private" {
     source = "terraform-aws-modules/ec2-instance/aws"
-    version = "6.15.0"
+    version = "6.1.1"
     
-    depends_on = [ module.vpc ]
+   depends_on = []
+   count = length(var.private_subnets)
+
 
     name = "${var.environment}-vm"
     ami = data.aws_ami.amazon_linux_2023.id
     instance_type = var.instance_type 
     key_name = var.instance_keypair
-    vpc_security_group_ids = [module.private_sg.security_group_id]
+    subnet_id = var.private_subnets[count.index]
+    vpc_security_group_ids = [var.private_sg_id]
     
-    instance_count = 2
-    subnet_ids = module.vpc.private_subnets
     
     tags = var.tags
 }
