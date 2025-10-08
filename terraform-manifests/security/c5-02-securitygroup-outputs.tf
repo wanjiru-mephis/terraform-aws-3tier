45135bc009684550a3e298e4e76624f4858aa602
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-08 17:18:04 +0300

    Fix security group outputs

--- terraform-manifests/security/c5-02-securitygroup-outputs.tf
+++ terraform-manifests/security/c5-02-securitygroup-outputs.tf
@@ -1,5 +1,5 @@
 # Public Bastion Host Security Group Outputs
-output "public_bastion_sg_group_id" {
+output "bastion_sg_id" {
   description = "The ID of the security group"
   value       = module.public_bastion_sg.security_group_id
 }
@@ -25,7 +25,7 @@ output "public_bastion_sg_group_description" {
 }
 
 # Private EC2 Instances Security Group Outputs
-output "private_sg_group_id" {
+output "private_sg_id" {
   description = "The ID of the security group"
   value       = module.private_sg.security_group_id
 }
