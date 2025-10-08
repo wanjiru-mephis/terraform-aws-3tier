95c882856d2d09bbda404387463ed3a358f7fc6b
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-08 17:29:43 +0300

    Add necessary modules

--- terraform-manifests/main.tf
+++ terraform-manifests/main.tf
@@ -1,35 +1,22 @@
-# Terraform Block
-terraform {
-    required_version = "~> 1.13.3"
-    required_providers {
-      aws = {
-        version = "~> 6.15.0"
-        source = "hashicorp/terraform-provider-aws"
-    }
-}
-}
-
-# Provider Block
-provider "aws" {
-    profile = "default"
-    region = "us-east-1" 
-}
-
-# === VPC Module ===
-module "three-tier-VPC" {
+module "three-tier-vpc" {
   source = "./vpc"
   tags   = local.common_tags
-
 }
-# === SECURITY MODULE ===
+
 module "security" {
   source = "./security"
   vpc_id = module.three-tier-vpc.vpc_id
   tags   = local.common_tags
 }
 
-# === EC2 MODULE ===
-module "EC2" {
-  source = "./ec2"
-  tags = local.common_tags
+module "ec2" {
+  source           = "./ec2"
+  environment      = var.environment
+  vpc_id           = module.three-tier-vpc.vpc_id
+  public_subnets   = module.three-tier-vpc.public_subnets
+  private_subnets  = module.three-tier-vpc.private_subnets
+  public_subnet_id = module.three-tier-vpc.public_subnets[0]
+  bastion_sg_id    = module.security.bastion_sg_id
+  private_sg_id    = module.security.private_sg_id
+  tags             = local.common_tags
 }
