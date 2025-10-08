b4b352d106b349b0392ea9222d2c3ab3f8a6ff4c
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-08 17:24:00 +0300

    Add missing variables

--- terraform-manifests/ec2/c6-02-ec2instance-variables.tf
+++ terraform-manifests/ec2/c6-02-ec2instance-variables.tf
@@ -6,6 +6,16 @@ variable "instance_type"{
     default = "t3.micro"
 }
 
+variable "public_subnets" {
+  description = "List of public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+  type        = list(string)
+}
+
 # AWS EC2 Instance Key Pair
 variable "instance_keypair"{
     description ="AWS EC2 Key pair"
@@ -16,4 +26,32 @@ variable "tags" {
   description = "Common tags applied to all resources in this VPC module"
   type        = map(string)
   default     = {}
+}
+
+# VPC ID
+variable "vpc_id" {
+  description = "VPC ID where EC2 instance will be launched"
+  type        = string
+}
+
+# Public Subnet ID (for Bastion host)
+variable "public_subnet_id" {
+  description = "Subnet ID for Bastion Host EC2 instance"
+  type        = string
+}
+
+# Bastion Security Group ID
+variable "bastion_sg_id" {
+  description = "Security Group ID for Bastion Host EC2 instance"
+  type        = string
+}
+
+variable "private_sg_id" {
+  description = "Security Group ID for Private EC2 instances"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g. dev, stage, prod)"
+  type        = string
 }
