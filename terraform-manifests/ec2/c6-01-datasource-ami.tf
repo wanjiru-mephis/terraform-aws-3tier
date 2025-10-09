7d8ae1579e0a72f32f68f9316deee9a7578b64bd
wanjiru-mephis <wanjirunduhi@gmail.com>
2025-10-09 19:23:25 +0300

    Fix mismatch between AMI architecture and EC2 instance type

--- terraform-manifests/ec2/c6-01-datasource-ami.tf
+++ terraform-manifests/ec2/c6-01-datasource-ami.tf
@@ -1,14 +1,16 @@
+# Get the latest Amazon Linux 2023 AMI for x86_64 (Intel/AMD)
 data "aws_ami" "amazon_linux_2023" {
-  most_recent      = true # Always picks the latest matching AMI
-  owners           = ["137112412989"]
+  most_recent = true
+  owners      = ["137112412989"] # Amazon official
 
   filter {
     name   = "name"
-    values = ["al2023-ami-minimal-*-arm64"]
+    values = ["al2023-ami-*-x86_64"]
   }
+
   filter {
     name   = "architecture"
-    values = ["arm64"]
+    values = ["x86_64"]
   }
 
   filter {
